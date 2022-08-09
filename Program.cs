00db4b129506287651d6b76b71ea7f344018fca8
Kevin <kevin.castro.chavez0@gmail.com>
2022-08-09 15:08:31 -0600

    used loops in collections (arrays)

--- Program.cs
+++ Program.cs
@@ -1,26 +1,53 @@
 ﻿class Program
 {
+    // public static void Main(string[] args)
+    // {
+    //     // Array of type int with space for 5 values
+    //     int[] grades = new int[5];
+
+    //     grades[0] = 20;
+    //     grades[1] = 12;
+    //     grades[2] = 23;
+    //     grades[3] = 25;
+    //     grades[4] = 04;
+
+    //     Console.WriteLine(grades[0]);
+
+    //     // Second way of declaring arrays
+    //     int[] gradesOfMathStudentsA = { 20, 12, 23, 25, 04 };
+
+    //     // Third way of declaring arrays
+    //     int[] gradesOfMathStudentsB = new int[] { 20, 12, 23, 25, 04, 40 };
+
+    //     Console.WriteLine(grades.Length);
+    //     Console.WriteLine(gradesOfMathStudentsA.Length);
+    //     Console.WriteLine(gradesOfMathStudentsB.Length);
+    // }
+
     public static void Main(string[] args)
     {
-        // Array of type int with space for 5 values
-        int[] grades = new int[5];
+        int[] nums = new int[10];
 
-        grades[0] = 20;
-        grades[1] = 12;
-        grades[2] = 23;
-        grades[3] = 25;
-        grades[4] = 04;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = i;
+        }
 
-        Console.WriteLine(grades[0]);
+        // for (int j = 0; j < nums.Length; j++)
+        // {
+        //     Console.WriteLine(nums[j]);
+        // }
 
-        // Second way of declaring arrays
-        int[] gradesOfMathStudentsA = { 20, 12, 23, 25, 04 };
+        foreach (int item in nums)
+        {
+            Console.WriteLine(item);
+        }
 
-        // Third way of declaring arrays
-        int[] gradesOfMathStudentsB = new int[] { 20, 12, 23, 25, 04, 40 };
+        string[] bestFriends = { "Citlalli", "Cindy", "Kevin", "Vanessa", "David"};
 
-        Console.WriteLine(grades.Length);
-        Console.WriteLine(gradesOfMathStudentsA.Length);
-        Console.WriteLine(gradesOfMathStudentsB.Length);
+        foreach (string friend in bestFriends)
+        {
+            Console.WriteLine(friend);
+        }
     }
 }
